c93f5ed7b65a4ffbf4e059a7a474810c2584e5b8
hunghuynh71 <hunghuynh813@gmail.com>
2023-05-17 23:19:22 +0700

    fix error: route.get() require callback funtion...

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,32 @@
 import module from 'module'
+// get the client
+//const mysql = require('mysql2');
+import sql from 'mysql2'
+const mysql = sql;
 
 let getHomePage = (req, res) => {
     //logic 
+    // create the connection to database
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'learn_nodejs'
+    });
+
+    // simple query
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            console.log(results); // results contains rows returned by server
+            //console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
 
     return res.render('index.ejs');
 }
 
-export default getHomePage;
+//dau '{}' giup nodejs hieu n la function
+export default { getHomePage };
 
 // module.exports = {
 //     getHomePage
