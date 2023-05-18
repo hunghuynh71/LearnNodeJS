4615977e88a8f13154ad2f1dba6998b874939feb
hunghuynh71 <hunghuynh813@gmail.com>
2023-05-18 22:23:37 +0700

    show data in table

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,14 +9,15 @@ let getHomePage = (req, res) => {
     connection.query(
         'SELECT * FROM `users`',
         function (err, results, fields) {
-            console.log('>>>>> Check mysql');
+            //console.log('>>>>> Check mysql');
             data = results.map((row) => { return row });
-            console.log(data);
+            //console.log(data);
 
-            //return res.render('index.ejs', { dataUsers: JSON.stringify(data) });
+            //dataUsers: JSON.stringify(data)
+            //return res.render('index.ejs', { data });
         })
 
-    return res.render('index.ejs', { dataUsers: JSON.stringify(data) });
+    return res.render('index.ejs', { data });
 }
 
 //dau '{}' giup nodejs hieu n la function
